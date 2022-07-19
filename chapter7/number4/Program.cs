e3fd7954474033e7769a9a2e64659f764c4dd218
Oluwaseun011 <oluwaseunidowu011@gmail.com>
2022-07-19 14:57:52 +0100

    challenges

---
+++ chapter7/number4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace number4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] sequence = { 1, 1, 2, 3, 2, 2, 2, 1 };
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
